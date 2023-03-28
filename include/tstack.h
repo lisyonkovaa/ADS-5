40f045961bca643b24c700df70880e21c0cc82f5
lisyonkovaa <114025777+lisyonkovaa@users.noreply.github.com>
2023-03-28 20:13:37 +0000

    13

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,36 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+  T arr[size];
+  int top;
+
+ public:
+  TStack():top(-1) {}
+  bool isEmpty() const {
+    return top == -1;
+  }
+  bool isFull() const {
+    return top == 0;
+  }
+  void push(T & value) {
+    if (isFull())
+      throw std::string("Full")
+    else
+      arr[++top] = value;
+  }
+  T pop() {
+    if(isEmpty()) 
+      throw std::string("Empty")
+    else 
+      return arr[top--]
+  }
+  T get() {
+    if(isEmpty()) 
+      throw std::string("Empty")
+    else 
+      return arr[top]
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
