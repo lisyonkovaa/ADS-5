dc0901c126ebc913e18beb62872be210a61bfa8e
lisyonkovaa <114025777+lisyonkovaa@users.noreply.github.com>
2023-03-30 07:33:47 +0000

    upd3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,77 +1,86 @@
-// Copyright 2021 NNTU-CS
-#include <ctype.h>
-#include <iostream>
 #include <string>
-#include <sstream>
-#include <cstdlib>
 #include <map>
 #include "tstack.h"
 
-int get_priority(char s) {
-    switch (s) {
-        case '(': return 0;
-        case ')': return 1;
-        case '+': return 2;
-        case '-': return 2;
-        case '*': return 3;
-        case '/': return 3;
-        default: return -1;
+int getPriority(char item) {
+    switch (item) {
+    case '(':
+        return 0;
+    case ')':
+        return 1;
+    case '+':
+        return 2;
+    case '-':
+        return 2;
+    case '*':
+        return 3;
+    case '/':
+        return 3;
     }
+    return -1;
 }
-
+bool isOper(char item) {
+    if (item >= '0' && item <= '9')
+        return true;
+    return false;
+}
 std::string infx2pstfx(std::string inf) {
     TStack<char, 100> stack;
-    std::stringstream ss;
-    for (int i = 0; i < inf.size(); i++) {
-        char c = inf[i];
-        if (isdigit(c)) {
-            ss << c;
-        } else {
-            ss << ' ';
-            if (c == '(') {
-                stack.push(c);
-            } else if (c == ')') {
-                while (stack.get() != '(') {
-                    ss << stack.pop() << ' ';
-                }
+    std::string postfx;
+    for (char item : inf) {
+        if (isOper(item)) {
+            postfx += item;
+            postfx += ' ';
+        } else if (getPriority(item) == 0) {
+            stack.push(item);
+        } else if (getPriority(item) == 1) {
+            while (getPriority(stack.get()) != 0) {
+                postfx += stack.get();
+                postfx += ' ';
                 stack.pop();
-            } else {
-                while (!stack.isEmpty() && \
-                get_priority(c) <= get_priority(stack.get())) {
-                    ss << stack.pop() << ' ';
-                }
-                stack.push(c);
             }
+            stack.pop();
+        } else {
+            while (!stack.isEmpty() && \
+                   (getPriority(item) <= getPriority(stack.get()))) {
+                postfx += stack.get();
+                postfx += ' ';
+                stack.pop();
+            }
+            stack.push(item);
         }
     }
     while (!stack.isEmpty()) {
-        ss << ' ' << stack.pop();
+        postfx += stack.get();
+        postfx += ' ';
+        stack.pop();
     }
-    return ss.str();
+    postfx.pop_back();
+    return postfx;
 }
 
-int eval(std::string post) {
-    TStack<int, 100> stack1;
-    std::stringstream ss(post);
-    std::string token;
-    while (ss >> token) {
-        if (isdigit(token[0])) {
-            int num = std::atoi(token.c_str());
-            stack1.push(num);
-        } else {
-            int b = stack1.pop();
-            int a = stack1.pop();
-            int sum = a + b;
-            int min = a - b;
-            int mult = a * b;
-            int del = a / b;
-            switch (token[0]) {
-                case '+': stack1.push(sum); break;
-                case '-': stack1.push(min); break;
-                case '*': stack1.push(mult); break;
-                case '/': stack1.push(del); break;
+
+int eval(std::string postfx) {
+    TStack<int, 100> stack;
+    for (char item : postfx) {
+        if (item >= '0' && item <= '9') {
+            stack.push(item - '0');
+        } else if (item == '+' || item == '-' || \
+                   item == '/' || item == '*') {
+            int b = stack.get();
+            stack.pop();
+            int a = stack.get();
+            stack.pop();
+            if (item == '+') {
+                stack.push(a + b);
+            } else if (item == '-') {
+                stack.push(a - b);
+            } else if (item == '*') {
+                stack.push(a * b);
+            } else if (item == '/') {
+                stack.push(a / b);
             }
         }
     }
-    return stack1.pop();
+    return stack.get();
 }
